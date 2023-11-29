a40c89380bb2789048cdf4ee1d4086515b42eff5
adamghill <adamghill@yahoo.com>
2023-11-28 21:42:36 -0500

    Add `python-utils`.

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from django.core.management import execute_from_command_line
 
 wsgi = initialize(
     GITHUB_PERSONAL_ACCESS_TOKEN=getenv("GITHUB_PERSONAL_ACCESS_TOKEN"),
+    INSTALLED_APPS=[
+        "compressor",
+        "django_unicorn",
+        "unicorn",
+    ],
 )
 
 if __name__ == "__main__":

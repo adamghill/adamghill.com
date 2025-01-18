3a3a8103c6fb1dffee92dca027ad8758797f15ea
adamghill <adamghill@yahoo.com>
2025-01-18 16:04:04 -0500

    More logging.

--- sites/app.py
+++ sites/app.py
@@ -13,10 +13,42 @@ COMPRESS_FILTERS = {
     "js": ["compressor.filters.jsmin.rJSMinFilter"],
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
+            "format": "[%(asctime)s][%(levelname)s] %(name)s %(filename)s:%(funcName)s:%(lineno)d | %(message)s",
+            "datefmt": "%H:%M:%S",
+        },
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "": {"handlers": ["console"], "level": "INFO", "propagate": False},
+    },
+}
+
 wsgi = initialize(
     GITHUB_PERSONAL_ACCESS_TOKEN=getenv("GITHUB_PERSONAL_ACCESS_TOKEN"),
     COMPRESS_FILTERS=COMPRESS_FILTERS,
     COMPRESS_ENABLED=True,
+    LOGGING=LOGGING,
 )
 
 if __name__ == "__main__":
